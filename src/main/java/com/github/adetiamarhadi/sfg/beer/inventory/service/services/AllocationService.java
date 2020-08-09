120118657f2364f1ed70276e7cc151b363b6c421
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-10 05:46:22 +0700

    Chapter 230 : Add Allocation Service

---
+++ src/main/java/com/github/adetiamarhadi/sfg/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,7 @@
+package com.github.adetiamarhadi.sfg.beer.inventory.service.services;
+
+import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
